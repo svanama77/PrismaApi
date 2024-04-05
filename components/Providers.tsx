dde80ee679495c59d4bdb3cd8cdb25a6e7264a9c
vsaisrujan <saisrujanv77@gmail.com>
2024-04-04 21:01:34 -0500

    env

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,10 +1,13 @@
 "use client"
 import React from 'react'
 import { ApolloClient, InMemoryCache, ApolloProvider, gql } from '@apollo/client';
+import { BASE_URL } from '@/config';
 
 export const Providers = ({children} :{children:React.ReactNode}) => {
+   const url = BASE_URL
+   console.log(url)
     const client = new ApolloClient({
-        uri: process.env.NEXT_SERVER_URL,
+        uri: `${BASE_URL}`,
         cache: new InMemoryCache(),
       });
 
